8752d045eb1a051ff6922c48dce199ee45c0f3a3
rizqimauana04 <mrizqim01@gmail.com>
2022-12-10 13:38:07 +0700

    Upload File

--- praktikum7.py
+++ praktikum7.py
@@ -1,10 +1,3 @@
-## **PROGRAM INPUT NILAI MAHASISWA MENGGUNAKAN OOP**
-
-### **PENGERTIAN OOP**
-- **object-oriented-programing/OOP** adalah paradigma pemrograman berdasarkan konsep "objek", yang dapat berisi data, dalam bentuk field atau dikenal juga sebagai atribut; serta kode, dalam bentuk fungsi/prosedur atau dikenal juga sebagai method.
-<br>
-
-```sh
 print('\033[1m','\33[34m')
 print("=" * 65)
 print("|\tPROGRAM INPUT NILAI MAHASISWA MENGGUNAKAN OOP\t|")
@@ -87,18 +80,3 @@ while True:
         exit()
     else:
         print("\33[31mPILIHAN MENU TIDAK ADA!")
-```
-<br>
-## **PENJELASAN**
-1. dekralarasi *dataMahasasiwa* sebagai object untuk menerima inputan data.
-2. deklarasi *class* di isi dengan method:
-- def tambah() di isi dengan inputan *nama, nim, tugas, uts, uas, akhir* , di variable akhir di isi dengan penjumlahan dan bagi 3, untuk tugas 3.5 untuk uts, 3.5 untuk uas. terakhir masukan semua kedalam object *dataMahasiswa*.
-
-- def tampil() untuk mencetak object *dataMahasiwa* serta gunakan perulangan *for* untuk mecetak semua data jika di dalam object  *dataMahasiwa* berisi banyak data.
-- def hapus(nama) untuk menghapus data di dalam parameter nama dan gunakan syntax *del*.
-- def ubah(nama) untuk mengubah data di dalam parameter nama  dan gunakan statment *if else nama di dalam dataMahasiswa* lalu masukan kembali ke dalam object *dataMahasiswa*.
-3. gunakan pengulangan *while True* di isikan dengan inputan menu, lalu di dalamnya di isikan dengan statment *if elif else*.
-4. di dalam statment *if elif else* di isikan dengan memanggil masing-masing method yang ada di dalam *class mahasiswa*.
-5. selesai
-## **FLOWCHART**
-![img](gambar/flowchart.png)
